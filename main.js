dd167a9e639e9554034d58dfe7f4b740a1725299
Wendhi Yesbel Angeles <126347757+YessYoongD@users.noreply.github.com>
2023-03-20 21:13:14 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+let app = document.getElementById('typewriter');
+ 
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+ 
+typewriter
+  .pauseFor(2500)
+  //cambiar el color de la frase que viene abajo de hola soy Yesbel
+  .typeString('<span style = "color: #4b3c48; ">Desarrollo sitios web y escribo artículos sobre programación.</span>')
+  .pauseFor(200)
+  .deleteChars(10)
+  .start();
+  
