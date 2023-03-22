4e33ce1afd49086155f9bbbd434f328cba71eaf7
Wendhi Yesbel Angeles <126347757+YessYoongD@users.noreply.github.com>
2023-03-22 16:40:59 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let typewriter = new Typewriter(app, {
 typewriter
   .pauseFor(2500)
   //cambiar el color de la frase que viene abajo de hola soy Yesbel
-  .typeString('<span style = "color: #4b3c48; ">Desarrollo sitios web y escribo artículos sobre programación.</span>')
+  .typeString('<span style = "color: #4b3c48; ">Soy UX designer y desarrollo sitios web.</span>')
   .pauseFor(200)
   .deleteChars(10)
   .start();
